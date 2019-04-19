03232648104029a1d2dded44c861a2193edf1f6d
zheleznyakov <zhelezny.andrey@gmail.com>
2019-04-19 19:14:42 +0300

    Пробный вариант верхнего меню

--- app/Views/Theme/layout.php
+++ app/Views/Theme/layout.php
@@ -12,7 +12,6 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -20,16 +19,40 @@
     <title>Hello, world!</title>
 </head>
 <body>
-<nav class="navbar navbar-expand navbar-dark bg-primary">
-    <!-- Navbar content -->
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link" href="#">Двигатели </a>
-            <a class="nav-item nav-link active" href="#">Сотрудники</a>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler01" >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarToggler01">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Моторы</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Пользователи</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline">
+                        <input class="form-control mr-2 my-2" type="text" placeholder="Логин" aria-label="Имя пользователя">
+                        <input class="form-control mr-2" type="password" placeholder="Пароль" aria-label="Пароль">
+                        <button class="btn btn-outline-success my-2" type="submit">Вход</button>
+                    </form>
+                </div>
+            </nav>
         </div>
     </div>
-</nav>
-<?=$content;?>
+    <div class="row">
+        <div class="col-lg-9" style="background: aqua">Content</div>
+        <div class="col-lg-3" style="background: beige">Sidebar</div>
+    </div>
+    <div class="row">
+        <div class="col" style="background: aquamarine">Footer</div>
+    </div>
+</div>
+
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
